aa92eb8f953e7ec1cf8771f1062bb1b4a09bcfed
pmbock1 <71155206+pmbock1@users.noreply.github.com>
2020-11-18 12:24:25 -0700

    Near Finished Website: Markers and Profile Pic
    Added a marker shape to search for (currently not working on desktop pc) and attempted a second method for image upload

--- upload.js
+++ upload.js
@@ -1,17 +1,4 @@
-function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
 
-            reader.onload = function (e) {
-                $('#blah')
-                    .attr('src', e.target.result)
-                    .width(150)
-                    .height(200);
-            };
-
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
 
      function randombg() {
 
@@ -31,3 +18,7 @@ function readURL(input) {
             randombg();
             
         }
+        var loadFile = function(event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
