833cb817ee337a1ddd34ec56cd53539a9a6d83d5
pmbock1 <71155206+pmbock1@users.noreply.github.com>
2020-11-18 12:35:29 -0700

    Removed unnecessary js librarys and bug fixed AR

--- upload.js
+++ upload.js
@@ -19,6 +19,6 @@
             
         }
         var loadFile = function(event) {
-    var image = document.getElementById('output');
-    image.src = URL.createObjectURL(event.target.files[0]);
-};
+            var image = document.getElementById('output');
+            image.src = URL.createObjectURL(event.target.files[0]);
+        };
